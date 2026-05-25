abe8421c64ae2483d0d90c5a10e3bb520c0b35a3
realhimanshujha <jhah895@gmail.com>
2026-05-25 22:22:13 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -856,10 +856,26 @@ def delete(id):
     return redirect("/")
 
 
+import os
+
 if __name__=="__main__":
 
     app.run(
+
     host="0.0.0.0",
-    port=5000,
-    debug=True
+
+    port=int(
+
+    os.environ.get(
+
+    "PORT",
+
+    5000
+
+    )
+
+    ),
+
+    debug=False
+
     )
